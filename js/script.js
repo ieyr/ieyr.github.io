b51bc429b2a8933da84f30e40d11a9aa659c21a4
Rohan Iyer <rohan.iyer1999@gmail.com>
2015-08-03 15:41:46 -0700

    huge additions to projects

--- js/script.js
+++ js/script.js
@@ -5,7 +5,8 @@ ref.child('porfolio').on('child_added', function(snapshot){
 	var link = value["link"]
 	var desc = value["description"]
 	var prize = value["prize"]
+	var color = value["color"]
 	console.log(name)
 	//$('#features').append('<div id="project"><div id="projecttitle"><a href="' + link + '">' + name+ '</a></div></div>')
-	$('#features').append('<div id="project"><div id="projecttitle"><a href="'+ link +'">' + name + '</a></div><p id="text">' + desc + ' ' + prize + '</p><a href="' + link + '"><button id="goToProject">LEARN MORE</button></a>	</div>')
+	$('#features').append('<div id="project" class="' + color + '"><div id="projecttitle"><a href="'+ link +'">' + name + '</a></div><p id="text">' + desc + ' ' + prize + '</p><div class="buttonDiv"><a href="' + link + '"><button id="goToProject">LEARN MORE</button></a></div></div>')
 })
